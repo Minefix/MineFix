1072ec1356c82f37bd6e8c3491254f092d751989
BuildTools <bart.ribbers@openmailbox.org>
2016-10-27 00:43:05 +0200

    Removed the 'fancy' bed, 'simple' bed renamed to just 'bed'

--- mods/beds/beds.lua
+++ mods/beds/beds.lua
@@ -1,58 +1,7 @@
--- Fancy shaped bed
-
-beds.register_bed("beds:fancy_bed", {
-	description = "Fancy Bed",
-	inventory_image = "beds_bed_fancy.png",
-	wield_image = "beds_bed_fancy.png",
-	tiles = {
-		bottom = {
-			"beds_bed_top1.png",
-			"default_wood.png",
-			"beds_bed_side1.png",
-			"beds_bed_side1.png^[transformFX",
-			"default_wood.png",
-			"beds_bed_foot.png",
-		},
-		top = {
-			"beds_bed_top2.png",
-			"default_wood.png",
-			"beds_bed_side2.png",
-			"beds_bed_side2.png^[transformFX",
-			"beds_bed_head.png",
-			"default_wood.png",
-		}
-	},
-	nodebox = {
-		bottom = {
-			{-0.5, -0.5, -0.5, -0.375, -0.065, -0.4375},
-			{0.375, -0.5, -0.5, 0.5, -0.065, -0.4375},
-			{-0.5, -0.375, -0.5, 0.5, -0.125, -0.4375},
-			{-0.5, -0.375, -0.5, -0.4375, -0.125, 0.5},
-			{0.4375, -0.375, -0.5, 0.5, -0.125, 0.5},
-			{-0.4375, -0.3125, -0.4375, 0.4375, -0.0625, 0.5},
-		},
-		top = {
-			{-0.5, -0.5, 0.4375, -0.375, 0.1875, 0.5},
-			{0.375, -0.5, 0.4375, 0.5, 0.1875, 0.5},
-			{-0.5, 0, 0.4375, 0.5, 0.125, 0.5},
-			{-0.5, -0.375, 0.4375, 0.5, -0.125, 0.5},
-			{-0.5, -0.375, -0.5, -0.4375, -0.125, 0.5},
-			{0.4375, -0.375, -0.5, 0.5, -0.125, 0.5},
-			{-0.4375, -0.3125, -0.5, 0.4375, -0.0625, 0.4375},
-		}
-	},
-	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
-	recipe = {
-		{"", "", "group:stick"},
-		{"wool:red", "wool:red", "wool:white"},
-		{"group:wood", "group:wood", "group:wood"},
-	},
-})
-
 -- Simple shaped bed
 
 beds.register_bed("beds:bed", {
-	description = "Simple Bed",
+	description = "Bed",
 	inventory_image = "beds_bed.png",
 	wield_image = "beds_bed.png",
 	tiles = {
@@ -66,7 +15,7 @@ beds.register_bed("beds:bed", {
 		},
 		top = {
 			"beds_bed_top_top.png^[transformR90",
-			"default_wood.png", 
+			"default_wood.png",
 			"beds_bed_side_top_r.png",
 			"beds_bed_side_top_r.png^[transformfx",
 			"beds_bed_side_top.png",
