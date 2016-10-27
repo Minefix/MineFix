2130ec19132bc136588b06c57d285d0214de2518
BuildTools <bart.ribbers@openmailbox.org>
2016-10-27 16:16:14 +0200

    Added basics of carts mod

---
+++ mods/minefix_carts/blocks.lua
@@ -0,0 +1,51 @@
+minetest.register_node("minefix_carts:rail", {
+	description = "Rail",
+	drawtype = "raillike",
+	tiles = {"minefix_carts_rail.png", "minefix_carts_rail_curved.png", "minefix_carts_rail_t_junction.png", "minefix_carts_rail_crossing.png"},
+	inventory_image = "minefix_carts_rail.png",
+	wield_image = "minefix_carts_rail.png",
+	paramtype = "light",
+	is_ground_content = true,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		-- but how to specify the dimensions for curved and sideways rails?
+		fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	groups = {bendy=2,snappy=1,dig_immediate=2,attached_node=1,rail=1,connect_to_raillike=1},
+})
+
+--[[minetest.register_node("minefix_carts:powerrail", {
+	description = "Powered Rail",
+	drawtype = "raillike",
+	tiles = {"minefix_carts_rail_pwr.png", "minefix_carts_rail_curved_pwr.png", "minefix_carts_rail_t_junction_pwr.png", "minefix_carts_rail_crossing_pwr.png"},
+	inventory_image = "minefix_carts_rail_pwr.png",
+	wield_image = "minefix_carts_rail_pwr.png",
+	paramtype = "light",
+	is_ground_content = true,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		-- but how to specify the dimensions for curved and sideways rails?
+		fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	groups = {bendy=2,snappy=1,dig_immediate=2,attached_node=1,rail=1,connect_to_raillike=1},
+
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.env:get_meta(pos):set_string("cart_acceleration", "0.5")
+		end
+	end,
+
+	mesecons = {
+		effector = {
+			action_on = function(pos, node)
+				minetest.env:get_meta(pos):set_string("cart_acceleration", "0.5")
+			end,
+
+			action_off = function(pos, node)
+				minetest.env:get_meta(pos):set_string("cart_acceleration", "0")
+			end,
+		},
+	},
+})]]--
--- mods/minefix_nether/README.md
+++ mods/minefix_nether/README.md
@@ -23,9 +23,9 @@ You should have received a copy of the GNU General Public License along with thi
 
 Media (textures and sounds) are licensed under [Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)](http://creativecommons.org/licenses/by-sa/3.0/).
 
-## Copyright:
+## Copyright
 
 Copyright (C) 2013 PilzAdam
 
-nether_rack.png: Zeg9
-nether_glowstone.png: BlockMen
+minefix_nether_rack.png: Zeg9
+minefix_nether_glowstone.png: BlockMen
